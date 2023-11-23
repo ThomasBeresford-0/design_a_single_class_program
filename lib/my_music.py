e57daaa57d24a61c7ad3a1e563d473ea81a58a39
Thomas Beresford <tomberesford.06@gmail.com>
2023-11-23 10:56:10 +0000

    added code to my my_music and test_my_music files

---
+++ lib/my_music.py
@@ -0,0 +1,13 @@
+class MyMusic:
+
+def __init__(self):
+#Empty dictionary to store our bands and tracks
+self.my_dict = {}
+
+def add_band(self, band, tracks):
+self.my_dict[band] = tracks
+print(f"Added band: {band} with tracks: {tracks}")
+
+def listened_to(self):
+print("I have listened to this so far")
+return self.my_dict
---
+++ tests/test_my_music.py
@@ -0,0 +1,38 @@
+from lib.my_music import MyMusic
+
+def test_add_to_dict():
+band_track_instance = MyMusic()
+band1 = "Metallica"
+tracks1 = "Enter Sandman"
+
+band_track_instance.add_band(band1, tracks1)
+
+assert band_track_instance.my_dict == {band1: tracks1}
+
+
+def test_add_to_dict_second_band():
+band_track_instance = MyMusic()
+band1 = "Metallica"
+tracks1 = "Enter Sandman"
+band2 = "ACDC"
+tracks2 = "Riff Raff"
+
+band_track_instance.add_band(band1, [tracks1])
+band_track_instance.add_band(band2, [tracks2])
+
+assert band_track_instance.my_dict == {band1: [tracks1], band2: [tracks2]}
+
+def test_add_to_dict_third_band():
+band_track_instance = MyMusic()
+band1 = "Metallica"
+tracks1 = "Enter Sandman"
+band2 = "ACDC"
+tracks2 = "Riff Raff"
+band3 = "Iron Maiden"
+tracks3 = "Prowler"
+
+band_track_instance.add_band(band1, [tracks1])
+band_track_instance.add_band(band2, [tracks2])
+band_track_instance.add_band(band3, [tracks3])
+
+assert band_track_instance.my_dict == {band1: [tracks1], band2: [tracks2], band3: [tracks3]}
